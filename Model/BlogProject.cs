923bc1f8e2dee6adc5c8c0f3dbdbb744ec5dcb2c
Michal Kuncl <michal.kuncl@gmail.com>
2014-12-12 15:25:34 +0100

    Model/BlogProject.cs - initial design for class that will represent blog project

---
+++ Model/BlogProject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MBlog.Model
+{
+    public class BlogProject
+    {
+        public string ProjectFile;
+        public string BlogName;
+        public string BlogDescription;
+        public string BlogAddress;
+        public string OutputPath;
+        public List<string> BlogFiles { get; set; }
+
+        public BlogProject()
+        {
+            BlogFiles = new List<string>();
+        }
+
+        public bool Load()
+        {
+            
+        }
+
+        public bool Save()
+        {
+            
+        }
+    }
+}
