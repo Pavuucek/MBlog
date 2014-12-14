efdf4dd6e71f35d01c68cf63213e565fa4514a84
Michal Kuncl <michal.kuncl@gmail.com>
2014-12-14 16:42:13 +0100

    Forms/MainForm.xaml.cs - updating preview and handling tooltips

--- Forms/MainForm.xaml.cs
+++ Forms/MainForm.xaml.cs
@@ -1,15 +1,56 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MarkdownDeep;
 
 namespace MBlog.Forms
 {
     /// <summary>
-    /// Interakční logika pro MainWindow.xaml
+    /// Interakční logika pro MainForm.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainForm
     {
-        public MainWindow()
+        public MainForm()
         {
             InitializeComponent();
         }
+        private readonly Markdown _markdown=new Markdown();
+        private string _markdownBuffer;
+
+        private void UpdatePreview()
+        {
+            _markdown.ExtraMode = true;
+            _markdown.SafeMode = false;
+            _markdownBuffer = _markdown.Transform(AvalonTextEditor.Text).Replace("\n", "\r\n");
+            PreviewWebBrowser.NavigateToString(_markdownBuffer);
+        }
+
+        private void TextEditor_TextChanged(object sender, System.EventArgs e)
+        {
+            UpdatePreview();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            EventManager.RegisterClassHandler(typeof(FrameworkElement), ToolTipOpeningEvent, new ToolTipEventHandler(ToolTipHandler));
+            EventManager.RegisterClassHandler(typeof(FrameworkElement),
+                  MouseLeaveEvent, new MouseEventHandler(ClearText));
+        }
+
+        private void ClearText(object sender, MouseEventArgs e)
+        {
+            TooltipTextBlock.Text = string.Empty;
+        }
+
+        private void ToolTipHandler(object sender, ToolTipEventArgs e)
+        {
+            e.Handled = true;
+            var source = e.Source as FrameworkElement;
+            if (source != null)
+            {
+                TooltipTextBlock.Text = source.ToolTip.ToString();
+            }
+        }
+
     }
 }
