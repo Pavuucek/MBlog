db02de5760bedd2c36a8bcf9d3e019690a060586
Michal Kuncl <michal.kuncl@gmail.com>
2014-12-16 21:59:37 +0100

    added class for Template

--- Model/BlogGenerator.cs
+++ Model/BlogGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using MarkdownDeep;
@@ -8,16 +9,30 @@ namespace MBlog.Model
 {
     public class BlogGenerator
     {
-        public BlogProject BlogProject { get; set; }
+        public BlogProject Project { get; set; }
+        public BlogTemplate Template { get; set; }
         public string OutputPath { get; set; }
+
+        private List<KeyValuePair<string, StringCollection>> _tagsBuffer;
+        private List<string> _tagsList;
         public BlogGenerator()
         {
-            
+            _tagsList = new List<string>();
+            _tagsBuffer = new List<KeyValuePair<string, StringCollection>>();
         }
 
         public void Generate()
         {
-            
+            // enumerate tags from post files
+            foreach (var blogFile in Project.BlogFiles)
+            {
+                var postfile = BlogPost.Load(blogFile.FullFileName);
+                foreach (var postTag in postfile.PostTags)
+                {
+                    var tag = _tagsBuffer.Where(kvp => kvp.Key == postTag);
+
+                }
+            }
         }
     }
 }
